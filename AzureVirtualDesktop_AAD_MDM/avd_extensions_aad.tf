420968fa8dd96c1417d4472c6acc7b06e0b8c755
Patrick van den Born <patrick@detechnischejongens.nl>
2023-10-03 20:51:53 +0200

    Updated extension

--- AzureVirtualDesktop_AAD_MDM/avd_extensions_aad.tf
+++ AzureVirtualDesktop_AAD_MDM/avd_extensions_aad.tf
@@ -6,40 +6,41 @@ locals {
 }
 
 resource "azurerm_virtual_machine_extension" "avd_register_session_host" {
-  count                = var.sessionhost_amount
-  name                 = "avd-${count.index + 1}-GOEUC-register-session-host"
-  virtual_machine_id   = azurerm_windows_virtual_machine.sessionhostvms.*.id[count.index]
-  publisher            = "Microsoft.Powershell"
-  type                 = "DSC"
-  type_handler_version = "2.73"
+  count                      = var.sessionhost_amount
+  name                       = "avd-${count.index + 1}-GOEUC-register-session-host"
+  virtual_machine_id         = azurerm_windows_virtual_machine.sessionhostvms.*.id[count.index]
+  publisher                  = "Microsoft.Powershell"
+  type                       = "DSC"
+  type_handler_version       = "2.73"
+  auto_upgrade_minor_version = true
 
-  settings = <<SETTINGS
+  settings = <<-SETTINGS
     {
-        "modulesUrl": "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_09-08-2022.zip",
-        "ConfigurationFunction": "Configuration.ps1\\AddSessionHost",
-        "Properties" : {
-          "hostPoolName" : "${azurerm_virtual_desktop_host_pool.hostpool.name}",
-          "aadJoin": true
-        }
-    }
-    SETTINGS
-
-  protected_settings = <<PROTECTED_SETTINGS
-    {
+      "modulesUrl": "https://wvdportalstorageblob.blob.core.windows.net/galleryartifacts/Configuration_09-08-2022.zip",
+      "configurationFunction": "Configuration.ps1\\AddSessionHost",
       "properties": {
-        "registrationInfoToken": "${azurerm_virtual_desktop_host_pool_registration_info.token.token}"
+        "HostPoolName":"${azurerm_virtual_desktop_host_pool.CAD-AVDHP.name}",
+        "aadJoin": true
       }
     }
-    PROTECTED_SETTINGS
+SETTINGS
 
-    depends_on = [
+  protected_settings = <<PROTECTED_SETTINGS
+  {
+    "properties": {
+      "registrationInfoToken": "${azurerm_virtual_desktop_host_pool_registration_info.token.token}"
+    }
+  }
+PROTECTED_SETTINGS
+
+  depends_on = [
     azurerm_virtual_desktop_host_pool.hostpool
   ]
-
   lifecycle {
-    ignore_changes = all
+    ignore_changes = [settings, protected_settings]
   }
 }
+
 
 resource "azurerm_virtual_machine_extension" "avd_aadloginforwindows" {
   depends_on = [
